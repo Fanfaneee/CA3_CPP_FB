150890b49de2b950e3849727f512920b9037ddd0
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-04-02 11:59:02 +0100

    crawler class

--- Crawler.cpp
+++ Crawler.cpp
@@ -1,5 +1,54 @@
-//
-// Created by fanfa on 02/04/2025.
-//
+#include <iostream>
+using namespace std;
 
+//#include "Board.h"
 #include "Crawler.h"
+
+
+Crawler::Crawler(int id, int x, int y, Direction dir, int size) {
+    this->id = id;
+    this->position = {x, y};
+    this->direction = dir;
+    this->size = size;
+    this->alive = true;
+
+    path.push_back(position);
+}
+
+void Crawler::move(int boardWidth, int boardHeight) {
+    if (!alive) return;
+
+
+    if (isWayBlocked(boardWidth, boardHeight)) {
+        do {
+            direction = static_cast<Direction>((rand() % 4) + 1);
+        } while (isWayBlocked(boardWidth, boardHeight));
+    }
+
+    switch (direction) {
+        case Direction::North:
+            if (position.y > 0) position.y--;
+        break;
+        case Direction::East:
+            if (position.x < boardWidth - 1) position.x++;
+        break;
+        case Direction::South:
+            if (position.y < boardHeight - 1) position.y++;
+        break;
+        case Direction::West:
+            if (position.x > 0) position.x--;
+        break;
+    }
+
+    path.push_back(position);
+}
+
+bool Crawler::isWayBlocked(int boardWidth, int boardHeight) const {
+    switch (direction) {
+        case Direction::North: return (position.y == 0);
+        case Direction::East:  return (position.x == boardWidth - 1);
+        case Direction::South: return (position.y == boardHeight - 1);
+        case Direction::West:  return (position.x == 0);
+    }
+    return false;
+}
--- Crawler.h
+++ Crawler.h
@@ -1,16 +1,38 @@
-//
-// Created by fanfa on 02/04/2025.
-//
-
 #ifndef CRAWLER_H
 #define CRAWLER_H
 
+#include <iostream>
+#include <list>
 
-
-class Crawler {
-
+struct Position {
+    int x;
+    int y;
 };
 
+enum class Direction {
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4
+};
+
+class Crawler {
+public:
+    int id;
+    Position position;
+    Direction direction;
+    int size;
+    bool alive;
+    std::list<Position> path;
 
 
-#endif //CRAWLER_H
+    Crawler(int id, int x, int y, Direction dir, int size);
+
+
+    void move(int boardWidth, int boardHeight);
+
+
+    bool isWayBlocked(int boardWidth, int boardHeight) const;
+};
+
+#endif // CRAWLER_H
